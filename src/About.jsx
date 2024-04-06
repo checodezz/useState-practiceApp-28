0acdd5459b700eaadb63c7bd83b0ea0082dc2303
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-06 05:38:03 +0000

    removed dead code

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,3 @@
-/* 
-2. Build a React component called About that accepts three props: heading, your name and learning. Inside the component, render a <h1> tag with the heading prop, <h2> tag with your name and a button below that which says “Know More”. On the click of the button, show the learning in a <p> tag just below the button.
-*/
-
 import { useState } from "react";
 
 const About = ({ heading, name, learning }) => {
